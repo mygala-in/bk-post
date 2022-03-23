b4ed663b518d59447918b9cc3ffb8edc7afa51b4
ksAvinash <justmailtoavi@gmail.com>
2022-03-23 08:15:09 +0530

    timeline service initial setup

--- timelineHandler.js
+++ timelineHandler.js
@@ -3,6 +3,20 @@ const logger = require('./bk-utils/logger');
 async function invoke(request) {
   logger.info('received timeline event');
   logger.info(JSON.stringify(request));
+  try {
+    const message = JSON.parse(request.Records[0].Sns.Message);
+    logger.info(message);
+    const { type } = message;
+    switch (type) {
+      case 'marriage.join':
+        break;
+      case 'marriage.remove':
+        break;
+      default:
+    }
+  } catch (err) {
+    logger.error(err);
+  }
 }
 
 module.exports = {
